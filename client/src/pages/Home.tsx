dcebb715e35676e01dab239817182ad8654b3bba
ramynbl <r_nebili@hetic.eu>
2026-02-23 15:39:09 +0100

    Update Prochain event

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -89,7 +89,7 @@ export default function Home() {
                   </div>
                   <div>
                     <p className="font-semibold">Horaire</p>
-                    <p className="text-foreground/70">!! PROCHAIN EVENT TRÈS BIENTÔT !!<br></br>Vous pouvez vous inscrire dès maintenant sur le site ! <br />Vous serez notifier par mail du prochain event.</p>
+                    <p className="text-foreground/70">Mardi 03 Mars 2026 - 17h30</p>
                   </div>
                 </div>
 
